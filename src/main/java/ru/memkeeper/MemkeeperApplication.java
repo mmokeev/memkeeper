8d3398b9f82cb7942b4848dfcb38cbac4ee804d1
Mikhail <kaduk.mikhail@gmail.com>
2020-04-04 21:43:16 +0500

    Начальный коммит

---
+++ src/main/java/ru/memkeeper/MemkeeperApplication.java
@@ -0,0 +1,13 @@
+package ru.memkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemkeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemkeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/memkeeper/MemkeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.memkeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemkeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
