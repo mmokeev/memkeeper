b3fb527173a98003ab4b1c41d49eaa62c0de7251
Mikhail <kaduk.mikhail@gmail.com>
2020-04-22 16:18:47 +0500

    Добавил конструкторы и геттеры для доменных объектов

--- src/main/java/ru/memkeeper/entities/Post.java
+++ src/main/java/ru/memkeeper/entities/Post.java
@@ -7,4 +7,22 @@ public class Post {
     private String header;
     private LocalDateTime dateTime;
     private String text;
+
+    public Post(String header, LocalDateTime dateTime, String text) {
+        this.header = header;
+        this.dateTime = dateTime;
+        this.text = text;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getText() {
+        return text;
+    }
 }
--- src/main/java/ru/memkeeper/entities/Tab.java
+++ src/main/java/ru/memkeeper/entities/Tab.java
@@ -6,4 +6,17 @@ public class Tab {
     private long id;
     private String name;
     private List<Post> posts;
+
+    public Tab(String name, List<Post> posts) {
+        this.name = name;
+        this.posts = posts;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
 }
